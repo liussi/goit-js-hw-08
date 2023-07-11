1e92b1aee83c62d0aad20ae43ca406b6001ca308
Liudmila-Ridko <125599487+liussi@users.noreply.github.com>
2023-07-11 20:59:08 +0300

    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({preview, original, description}) => `<li class="gallery__item">
+<a class="gallery__link" onclick="return false" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}"  />
+</a>
+</li>
+`);
+galleryEl.insertAdjacentHTML('beforeend', markup.join(''));
+
+let lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionDelay : 250,
+ });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
 
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+    const onPlay = function( data) {  
+    };
+    
+    player.on('timeupdate', onPlay);
+    player.off('timeupdate', onPlay);
+

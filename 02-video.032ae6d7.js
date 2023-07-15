84471e79c8b4c206c7796977560df8afe31c80a0
liussi <125599487+liussi@users.noreply.github.com>
2023-07-15 08:39:37 +0000

    Deploying to gh-pages from @ liussi/goit-js-hw-08@3f61963e37c74a7f6343bcd3ad3e5dcd75c14380 🚀

--- 02-video.032ae6d7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)}));var t=function(e){};o.on("timeupdate",t),o.off("timeupdate",t)}();
-//# sourceMappingURL=02-video.032ae6d7.js.map
